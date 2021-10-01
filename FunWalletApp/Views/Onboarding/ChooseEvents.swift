93c6ec18af671b2e2ab0826f241d55a287a2c86e
jkobb510 <41555063+jkobb510@users.noreply.github.com>
2021-10-01 19:23:46 -0400

    Updated ChooseEvents

--- FunWalletApp/Views/Onboarding/ChooseEvents.swift
+++ FunWalletApp/Views/Onboarding/ChooseEvents.swift
@@ -8,9 +8,21 @@
 import UIKit
 
 class ChooseEvents: UIViewController {
+    enum Segues {
+        static let toFirstChild = "toFirstChild"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func prepare (for segue: UIStoryboardSegue, sender: Any?){
+        if segue.identifier == Segues.toFirstChild {
+            let destVC = segue.destination as! FirstChildVC
+            
+        }
+    }
+
     
     @IBAction func Submit(_ sender: UIButton) {
         performSegue(withIdentifier: K.segue.showMain2, sender: nil)
---
+++ FunWalletApp/Views/Onboarding/FirstChildVC.swift
@@ -0,0 +1,35 @@
+//
+//  FirstChildVC.swift
+//  FunWalletApp
+//
+//  Created by Seung Min Lee on 9/25/21.
+//
+
+import UIKit
+
+class FirstChildVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBOutlet weak var Star1: UIImageView!
+    @IBOutlet weak var Star2: UIImageView!
+    @IBOutlet weak var Star3: UIImageView!
+    @IBOutlet weak var Star4: UIImageView!
+    @IBOutlet weak var Star5: UIImageView!
+    @IBOutlet weak var EventTitle: UILabel!
+    
+    @IBOutlet weak var EventImage: UIImageView!
+    @IBOutlet weak var EventDescription: UILabel!
+    
+    @IBOutlet weak var HostName: UILabel!
+    
+    @IBOutlet weak var HostImage: UIImageView!
+    
+    @IBOutlet weak var G_Symbol: UILabel!
+    
+    @IBOutlet weak var GroupDateAlert: UILabel!
+}
+
+

5af9e5ccdd6a75178ee0aed86ccecdf6dc7e5987
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2022-01-31 12:42:32 -0500

    Update Onboarding.swift

--- funwallet2022/Onboarding.swift
+++ funwallet2022/Onboarding.swift
@@ -1,12 +1,5 @@
-//
-//  Onboarding.swift
-//  funwallet2022
-//
-//  Created by Seung (Jacob) Lee on 1/21/22.
-//
-
 import SwiftUI
-
+import GoogleSignIn
 
 
 struct Onboarding_Previews : PreviewProvider {
@@ -16,74 +9,51 @@ struct Onboarding_Previews : PreviewProvider {
 }
 
 struct Onboarding: View {
-    @State fileprivate var isShowingSheet: Bool = false
-    @State private var showThirdView = false
+    @EnvironmentObject var viewModel: AuthenticationViewModel
+    
+    // 2
+    private let user = GIDSignIn.sharedInstance.currentUser
 
     @State var CurrentIndex : Int = 1
     var body: some View {
-        VStack {
-            ZStack{
-            Image("LaunchIcon")
-            .renderingMode(.none).resizable()
-            .aspectRatio(contentMode: .fill)
-            .padding(.top, 7.0)
-            .frame(alignment: .top)
-            .frame(width: 50.0, height: 50.0)
-            }
-            
-            TabView(selection: $CurrentIndex) {
-                ForEach(1...3,id: \.self) {index in
-                    //Custom Scroll Effect
-                    GeometryReader{proxy -> AnyView in
-                        let minX = proxy.frame(in: .global).minX
-                        let width = UIScreen.main.bounds.width
-                        let progress = -minX / (width * 2)
-                        var scale = progress > 0 ? 1 - progress : 1 + progress
-                            scale = scale < 0.5 ? 0.7 : scale
-                         
-                        return AnyView(
-                            VStack{
-                                Image("pic\(index)")
-                                    .renderingMode(.none)
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(alignment: .leading)
-                                    .padding(.horizontal,2)
-                                CustomeTabIndicator(count: 3, current: $CurrentIndex).padding(.all, 1.0)
-                            }
-                                .frame(maxHeight: .infinity, alignment: .center)
-                            .scaleEffect(scale)
-                        )
-                    }
-                    .tag(index)
-                }
-            }
-            
-        
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-            //custom tab indicator
-            
-           getstarted()
-            
-        }//VStack
-        .frame(maxWidth: .infinity)
-        .background(Color.white).ignoresSafeArea()
-        }
-    
-}
-struct SecondView: View {
-    @Environment(\.presentationMode) var presentationMode
-    @Binding var showNext: Bool
+        NavigationView{
+            VStack {
 
-    var body: some View {
-        Button("Show the third view.") {
-            self.presentationMode.wrappedValue.dismiss()
-            DispatchQueue.main.async {
-                self.showNext = true
-            }
+            Image_And_Button()
+
+                VStack{ //Button
+                    NavigationLink(destination: LoginView()){
+                        
+                    Text("Get Started")
+                            .font(.title3).fontWeight(.bold).foregroundColor(.white).frame(maxWidth: 150, alignment: .center).padding(.vertical,12.0).padding(.horizontal, 20.0).background(RoundedRectangle(cornerRadius: 10).fill(Color.yellow)
+                               .overlay(RoundedRectangle(cornerRadius: 10) .stroke(Color.black,lineWidth: 1)
+                               ))}.padding([.leading, .bottom, .trailing], 40.0)
+                    
+                    
+                  
+                
+  
+                }
+                
+                
+            }.navigationBarTitleDisplayMode(.inline)
+                .toolbar { ToolbarItem(placement: .principal) {
+                        HStack {
+                            Image("LaunchIcon").resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0)).frame(width: 50.0, height: 50.0)
+                            Text("FunWallet").font(.headline).foregroundColor(Color.yellow)
+                        }.padding(.bottom, 10)
+                    }
+                }
+        .frame(maxHeight: .infinity)
+        .background(Color(red: 239.0/255.0, green: 240.0/255.0, blue: 240.0/255.0, opacity: 1.0)).ignoresSafeArea()
         }
     }
 }
+
+
+
+
+
 struct CustomeTabIndicator: View {
     var count: Int
     @Binding var current: Int
@@ -113,33 +83,50 @@ struct CustomeTabIndicator: View {
     }
 }
 
-struct getstarted: View {
-    @State fileprivate var isShowingSheet: Bool = false
-    var body: some View {
-        
-              Button(action: {signUp()}){
-              Text("Get Started")
-               .font(.title3).fontWeight(.bold).foregroundColor(.white)
-               .frame(maxWidth: 150, alignment: .center)
-             
-               .padding(.vertical,12.0).padding(.horizontal, 20.0)
-                     .background(
-                         RoundedRectangle(cornerRadius: 10)
-                             .fill(Color.yellow)
-                             .overlay(
-                                 RoundedRectangle(cornerRadius: 10)
-                                     .stroke(Color.black,lineWidth: 1)
-                             ))
-              }.padding([.leading, .bottom, .trailing], 40.0)
-             .sheet(isPresented: $isShowingSheet, content: {
-                 LoginView()
-                   })
-                  /* .background(
-                                
-                   NavigationLink(destination: Home(), isActive: $showThirdView) {
-                                      EmptyView()
-                                }
-                          )*/
+
+struct Image_And_Button: View {
+    @State var CurrentIndex : Int = 1
+var body: some View{
+    TabView(selection: $CurrentIndex) {
+        ForEach(1...3,id: \.self) {index in
+            //Custom Scroll Effect
+            GeometryReader {proxy -> AnyView in
+                let minX = proxy.frame(in: .global).minX
+                let width = UIScreen.main.bounds.width
+                let progress = -minX / (width * 2)
+                var scale = progress > 0 ? 1 - progress : 1 + progress
+                scale = scale < 1.0 ? 1.0 : scale
+                 
+                return AnyView(
+                        //Image("")
+                        VStack{
+   
+                        Image("pic\(index)")
+                            .renderingMode(.none)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .padding(.top, 10.0)
+                            
+                        }
+
+                        .frame(maxHeight: .infinity, alignment: .top)
+                    .scaleEffect(scale)
+                )
+            }//)
+            .tag(index)
         }
-    func signUp() { isShowingSheet = true }
+    }
+    
+    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+    //custom tab indicator
+
+
+    CustomeTabIndicator(count: 3, current: $CurrentIndex)
+    Text("Turn dating into an adventure. Discover hosted events near you")
+        .font(.title2)
+        .fontWeight(.regular)
+        .foregroundColor(Color.black).padding()
 }
+
+}
+
